f4b6dc22c7e793312bd8dc0f71511979db4ac9d0
dotan9 <dotan9@gmail.com>
2022-11-30 23:14:55 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 radio.onReceivedNumber(function (receivedNumber) {
     start = input.runningTimeMicros()
     if (receivedNumber == 5) {
+        k_Bit.LED_brightness(210)
+        k_Bit.Led(COLOR.red)
+        basic.showIcon(IconNames.Ghost)
         k_Bit.run(DIR.RunForward, 50)
     } else if (receivedNumber == 1) {
         k_Bit.carStop()
@@ -8,9 +11,10 @@ radio.onReceivedNumber(function (receivedNumber) {
 })
 let elapsed = 0
 let start = 0
+k_Bit.OFFLed()
+basic.clearScreen()
 start = 0
 radio.setGroup(1)
-basic.showIcon(IconNames.Ghost)
 basic.forever(function () {
     if (start > 0) {
         elapsed = input.runningTimeMicros() - start
